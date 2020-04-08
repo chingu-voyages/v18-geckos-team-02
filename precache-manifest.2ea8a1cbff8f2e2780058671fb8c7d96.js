69f1ba819b56080e943f6ad63bec437ce9b6cd3c
Solomon Ekrebe <krebedev@gmail.com>
2020-04-08 13:55:15 +0100

    Updates

---
+++ precache-manifest.2ea8a1cbff8f2e2780058671fb8c7d96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933ff7a272c1f9172308d16420640028",
+    "url": "/v18-geckos-team-02/index.html"
+  },
+  {
+    "revision": "8761342379e9ce57ff2c",
+    "url": "/v18-geckos-team-02/static/js/2.e908b219.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/v18-geckos-team-02/static/js/2.e908b219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817501a15f3de2d7b4c7",
+    "url": "/v18-geckos-team-02/static/js/main.adff7997.chunk.js"
+  },
+  {
+    "revision": "2c8287a754bf45e75d20",
+    "url": "/v18-geckos-team-02/static/js/runtime-main.f11fd062.js"
+  }
+]);
