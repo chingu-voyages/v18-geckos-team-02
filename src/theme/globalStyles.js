6b2d76dfb1813a7e231964b2aff7b9f875872e65
Solomon Ekrebe <krebedev@gmail.com>
2020-04-09 19:13:19 +0100

    add named-colors

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -17,5 +17,16 @@ body {
   font-size: 16px;
 }
 `;
-
 export default GlobalStyle;
+
+export const theme = {
+  red: "#FC5F6B",
+  blue: "#5887F9",
+  veryDarkBlue: "#4C5264",
+  lightBlue: "#60C3FF",
+  blueGradient: "#5574f7-#60C3FF",
+  grey: "#BCC5D3",
+  lightGrey: "#E2E8ED",
+  white: "#FFFFFF",
+  offWhite: "#F6F6F6"
+}
