3d616d975707b734e57589a3e88a34dd228dd2e5
Lily <46561378+lily-law@users.noreply.github.com>
2020-04-09 17:08:45 +0100

    Layout file structure

---
+++ src/components/UploadModal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UploadItem from './UploadItem';
+import DateAndTagsEditor from './DateAndTagsEditor';
+
+function UploadModal() {
+  return (
+    <>
+      UploadModal
+    </>
+  );
+}
+
+export default UploadModal;
