324249205735827697d326d323d6b5755cbf9e0a
Lily <46561378+lily-law@users.noreply.github.com>
2020-04-10 11:07:48 +0100

    Revert Move Timeline into NavBar (#22)
    * Layout file structure
    
    * Move Timeline into NavBar
    
    * Revert "Move Timeline into NavBar"
    
    This reverts commit 974c4eef32c94920ce7455435a0e8c75d247b90a.
    
    * Timeline open/close props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Main from './components/Main';
+import Timeline from './components/Timeline';
 import NavBar from './components/NavBar';
 import UploadModal from './components/UploadModal';
 import AddNoteModal from './components/AddNoteModal';
@@ -7,12 +8,14 @@ import AddNoteModal from './components/AddNoteModal';
 function App() {
   const [noteModalOpen, setNoteModalOpen] = useState(false);
   const [uploadModalOpen, setUploadModalOpen] = useState(false);
+  const [timelineOpen, setTimelineOpen] = useState(false);
   return (
     <>
       {uploadModalOpen && !noteModalOpen && <UploadModal close={() => setNoteModalOpen(false)} />}
       {noteModalOpen && <AddNoteModal close={() => setUploadModalOpen(false)} />}
       <Main />
-      <NavBar openModal={() => setNoteModalOpen(true)} openNote={() => setUploadModalOpen(true)} />
+      {!uploadModalOpen && !noteModalOpen && timelineOpen && <Timeline close={() => setTimelineOpen(false)} />}
+      <NavBar openModal={() => setNoteModalOpen(true)} openNote={() => setUploadModalOpen(true)} openTimeline={() => setTimelineOpen(true)} />
     </>
   );
 }
