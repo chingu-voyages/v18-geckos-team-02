0eee34217e9a317421e02123dd6d1e9d8fd0b20b
Lily <46561378+lily-law@users.noreply.github.com>
2020-04-10 14:07:36 +0100

    Make FileObj constructor

---
+++ src/services/FileObj.js
@@ -0,0 +1,50 @@
+if (!URL) { // polyfill for running test
+    var URL = {
+        createObjectURL: file => JSON.stringify(file)
+    }
+}
+
+export default function FileObj(file, tags, activeTimeStamp, timeStamps) {
+    const fileObj = {
+        fileType: this.detectFileType(file),
+        timeStamps: {},
+        activeTimeStamp,
+        tags,
+        formatDate: this.formatDate,
+        detectFileType: this.detectFileType
+    }
+    fileObj.file = fileObj.fileType === 'Note' || fileObj.fileType === 'unknown' ? file : URL.createObjectURL(file);
+    Object.keys(timeStamps).forEach(key => fileObj.timeStamps[key] = this.formatDate(timeStamps[key]));
+    return fileObj
+}
+FileObj.prototype.formatDate = function(dateTime) {
+    const makeMinTwoDigits = n => {
+        const str = n+'';
+        return str.length === 1 ? '0'+str : str
+    };
+    const d = new Date(dateTime);
+    return d.getFullYear() +
+        makeMinTwoDigits(d.getMonth()+1) +
+        makeMinTwoDigits(d.getDate()) +
+        makeMinTwoDigits(d.getHours()) +
+        makeMinTwoDigits(d.getMinutes()) +
+        makeMinTwoDigits(d.getSeconds());
+}
+FileObj.prototype.detectFileType = function(file) {
+    if (file instanceof File) {
+        if (file.hasOwnProperty('type')) {
+            return file.type
+        }
+        else {
+            return 'File'
+        }
+    }
+    else {
+        if (typeof file === 'string') {
+            return 'Note'
+        }
+        else {
+            return 'unknown'
+        }
+    }
+}
