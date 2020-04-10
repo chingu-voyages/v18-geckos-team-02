92df9c51e5bbc03bee3dc9b74ab74beb55f27350
Lily <46561378+lily-law@users.noreply.github.com>
2020-04-10 16:08:26 +0100

    Code basic idea for timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { listNodes } from '../services/dataController';
 
 function Timeline() {
+  const [scope, setScope] = useState({from: '0', to: '9'});
+  const nodes = listNodes(scope);
   return (
-    <>
-      Timeline
-    </>
+    <div>
+      <button>back</button>
+      {Object.keys(nodes).map(year => <div key={year}>
+        <h1>{year}</h1>
+        {Object.keys(nodes[year]).map(month => <div key={year+month}>
+          <h2>{month}</h2>
+          {Object.keys(nodes[year][month]).map(date => <div key={year+month+date}>
+            <h3>{date}</h3>
+            {nodes[year][month][date].map(fileObj => <div key={fileObj.key}>
+              {fileObj.fileType}
+            </div>)}
+          </div>)}
+        </div>)}
+      </div>)}
+      <button>forwards</button>
+    </div>
+    
   );
 }
 
