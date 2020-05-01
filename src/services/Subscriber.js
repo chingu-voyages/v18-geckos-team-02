98f2c73a09be75dfcd089f29c0220a91f4a0ac09
Lily <46561378+lily-law@users.noreply.github.com>
2020-05-01 12:02:58 +0100

    Refactor/datacontroller (#93)
    * Give delete persistence
    
    * Rename DataController to dataController
    
    * Refactor dataController
    - Move start into AppData as init
    - Change storage to localStorage
    
    * Change data consumption to subscribe methods

---
+++ src/services/Subscriber.js
@@ -0,0 +1,7 @@
+function Subscriber(stateSetter) {
+    this.stateSetter = stateSetter;
+    this.update = this.update.bind(this);
+}
+Subscriber.prototype.update = function(data) {
+    this.stateSetter(data);
+}
---
+++ src/services/dataController.js
@@ -0,0 +1,128 @@
+import AppData from './AppData';
+import FileObj from './FileObj';
+import errorHandler from './errorHandler';
+import { writeFile, readFile, writeAppData } from './localStorage';
+import Subscriber from './Subscriber';
+
+const appData = new AppData(handleFileObjsChange);
+function handleFileObjsChange() {
+    setActiveFileObjs();
+    setNodesList();
+}
+
+let activeNode;
+function setActiveNode(fileObj) {
+    if (fileObj.hasOwnProperty('getActiveDate')) {
+        activeNode = fileObj.getActiveDate().substr(0, 8);
+        setActiveFileObjs();
+    }
+    else {
+        errorHandler(Error(`Unable to setActiveNode with ${fileObj}`));
+    }
+}
+const nodesListSubcribers = [];
+function subscribeNodesList(setNodesList) {
+    nodesListSubcribers.push(new Subscriber(setNodesList));
+}
+function setNodesList() {
+    const getPath = dateTime => [dateTime.substr(0,4), ...dateTime.substr(4).match(/.{2}/g)]; 
+    const fileObjs = Object.values(appData.fileObjs);
+    const nodes = {};
+    for (let fileObj of fileObjs) {
+        const path = getPath(fileObj.getActiveDate().substr(0, 8));
+        let head = nodes;
+        for (let key of path) {
+            if (!head.hasOwnProperty(key)) {
+                if (key !== path[path.length-1]) {
+                    head[key] = {};
+                }
+                else {
+                    head[key] = [];
+                }
+            }
+            head = head[key];
+        }
+        head.push(fileObj);
+    }
+    nodesListSubcribers.forEach(sub => sub.update(nodes));
+}
+const activeFileObjsSubcribers = [];
+function subscribeActiveFileObjs(setFileObjs) {
+    nodesListSubcribers.push(new Subscriber(setFileObjs));
+}
+function getFileObjs(from = '0', to = '0') {
+    return Object.values(appData.fileObjs).filter(fileObj => {
+        const key = fileObj.getActiveDate();
+        return (key.substr(0, from.length) >= from && key.substr(0, to.length) <= to)
+    }).sort((a, b) => a.getActiveDate() - b.getActiveDate());
+}
+function setActiveFileObjs() {
+    const activeFileObjs = getFileObjs(activeNode);
+    activeFileObjsSubcribers.forEach(sub => sub.update(activeFileObjs));
+}
+
+async function addFiles(uploadsArr) {
+    let fileObj;
+    for (let upload of uploadsArr) {
+        const ref = makeFileRef(upload.file);
+        const {name, text = '', type} = upload.file;
+        const checkedType = checkFileType(type, name);
+        fileObj = new FileObj({...upload, name, text, fileRef: ref, type: checkedType});
+        const activeDate = fileObj.getActiveDate();
+        const fObjsWithSameFile = Object.values(appData.fileObjs).filter(fObj => fObj.fileRef === ref).map(fObj => fObj.getActiveDate());
+        if (fObjsWithSameFile.filter(date => date === activeDate).length === 0) {
+            appData.fileObjs[fileObj.uid] = fileObj;
+        }
+        if (fObjsWithSameFile.length < 1) {
+            await writeFile(ref, upload.file);
+        }
+    }
+    await writeAppData(appData);
+    return true
+}
+
+async function getFile(fileRef) {
+    try {
+        let file = await readFile(fileRef);
+        if (!file) {
+            // file = await - get from cloud/Google drive
+        }
+        if (!file) {
+            throw new Error(`File with ref: ${fileRef} not found!`)
+        }
+        return file
+    }
+    catch (e) {
+        errorHandler(e);
+    }
+}
+
+function removeFiles(fileObjs) {
+    fileObjs.forEach(fileObj => {
+        let key = fileObj.uid;
+        if (appData.fileObjs[key]) {
+        appData.delete(key);
+        return true
+    }
+    return Error(`no such file under key: ${key}`)
+    })
+}
+
+function makeFileRef(file) {
+    return file.name+file.size+file.lastModified
+}
+
+function checkFileType(type, name) {
+    if (!type) {
+        const segs = name.split('.');
+        if (segs.length > 1) {
+            return segs[segs.length-1];
+        }
+        else {
+            return "unknown"
+        }
+    }
+    return type
+}
+
+export {addFiles, getFile, removeFiles, subscribeNodesList, subscribeActiveFileObjs, setActiveNode}
