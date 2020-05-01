ddd1d2973d9274afca203f5854383e68cc96de87
Lily <46561378+lily-law@users.noreply.github.com>
2020-05-01 10:14:57 +0100

    Refactor dataController - Move start into AppData as init - Change storage to localStorage

--- src/services/AppData.js
+++ src/services/AppData.js
@@ -1,18 +1,28 @@
-import { writeAppData } from './storage';
+import {  readAppData, writeAppData } from './localStorage';
 export default function AppData(statusSubcriber) {
     this.setStatus = statusSubcriber;
     this.lastModified = null;
     this.basedOn = null;
     this.changeLog = [];
     this.fileObjs = {};
+    this.init();
+}
+AppData.prototype.init = async function() {
+    const data = await readAppData();
+    if (data) {
+        this.rebase(data);
+      //  setActiveNode(findFirstFileObj().getActiveDate().substr(0,8));
+    }
 }
 AppData.prototype.add = function(fileObj, log = true) {
     log && this.changeLog.push({ func: 'add', data: fileObj });
     this.fileObjs[fileObj.uid] = fileObj;
+    writeAppData(this);
 }
 AppData.prototype.update = function(fileObj, log = true) {
     log && this.changeLog.push({ func: 'update', data: fileObj });
     Object.assign(this.fileObjs[fileObj.uid], fileObj);
+    writeAppData(this);
 }
 AppData.prototype.delete = function(uid, log = true) {
     log && this.changeLog.push({ func: 'delete', data: uid });
@@ -26,4 +36,5 @@ AppData.prototype.rebase = function(newAppData) {
         this[op.func](op.data, false);
     }
     this.changeLog = [];
+    writeAppData(this);
 }
--- src/services/dataController.js
+++ src/services/dataController.js
@@ -1,36 +1,11 @@
 import AppData from './AppData';
 import FileObj from './FileObj';
 import errorHandler from './errorHandler';
-import { writeFile, readFile, readAppData, writeAppData } from './storage';
-export default function DataController(statusSubcriber, nodeListSubcriber, setActiveNode) {
-    this.appData = new AppData(statusSubcriber);
-    this.setStatus = statusSubcriber;
-    this.nodeListSubcriber = nodeListSubcriber;
-    this.setActiveNode = setActiveNode;
-    this.addFiles = this.addFiles.bind(this);
-    this.listNodes = this.listNodes.bind(this);
-    this.getFileObjs = this.getFileObjs.bind(this);
-    this.getFile = this.getFile.bind(this);
-    this.start = this.start.bind(this);
-    this.removeFiles = this.removeFiles.bind(this);
-}
-DataController.prototype.start = async function() {
-    const data = await readAppData('local');
-    if (data) {
-        this.appData.rebase(data);
-        this.updateList();
-        this.setActiveNode(this.findFirstFileObj().getActiveDate().substr(0,8));
-    }
-}
-DataController.prototype.syncAppData = async function() {
-    // if client basedOn date === cloud lastModified date --> replace cloud data with client data
-    // if client basedOn date !== cloud lastModified date --> pull in new cloud data --> run each operation from log on new data --> clear log --> compare client basedOn date === cloud lastModified date again
-}
-DataController.prototype.updateList = function() {
-    this.nodeListSubcriber(this.listNodes());
-}
-DataController.prototype.addFiles = async function(uploadsArr) {
-    this.setStatus('Uploading files');
+import { writeFile, readFile, writeAppData } from './localStorage';
+
+const appData = new AppData();
+
+async function addFiles(uploadsArr) {
     let fileObj;
     for (let upload of uploadsArr) {
         const ref = makeFileRef(upload.file);
@@ -38,25 +13,20 @@ DataController.prototype.addFiles = async function(uploadsArr) {
         const checkedType = checkFileType(type, name);
         fileObj = new FileObj({...upload, name, text, fileRef: ref, type: checkedType});
         const activeDate = fileObj.getActiveDate();
-        const fObjsWithSameFile = Object.values(this.appData.fileObjs).filter(fObj => fObj.fileRef === ref).map(fObj => fObj.getActiveDate());
+        const fObjsWithSameFile = Object.values(appData.fileObjs).filter(fObj => fObj.fileRef === ref).map(fObj => fObj.getActiveDate());
         if (fObjsWithSameFile.filter(date => date === activeDate).length === 0) {
-            this.appData.fileObjs[fileObj.uid] = fileObj;
+            appData.fileObjs[fileObj.uid] = fileObj;
         }
         if (fObjsWithSameFile.length < 1) {
-            this.setStatus('Uploading '+upload.file.name);
             await writeFile(ref, upload.file);
         }
     }
-    this.updateList();
-    this.setActiveNode(fileObj.getActiveDate().substr(0,8));
-    this.setStatus('Storing updates locally');
-    await writeAppData(this.appData);
-    this.setStatus('');
+    await writeAppData(appData);
     return true
 }
-DataController.prototype.listNodes = function() {
+function listNodes() {
         const getPath = dateTime => [dateTime.substr(0,4), ...dateTime.substr(4).match(/.{2}/g)]; 
-        const fileObjs = Object.values(this.appData.fileObjs);
+        const fileObjs = Object.values(appData.fileObjs);
         const nodes = {};
         for (let fileObj of fileObjs) {
             const path = getPath(fileObj.getActiveDate().substr(0, 8));
@@ -76,14 +46,14 @@ DataController.prototype.listNodes = function() {
         }
         return nodes
 }
-DataController.prototype.getFileObjs = function(from = '0', to = '0') {
-    return Object.values(this.appData.fileObjs).filter(fileObj => {
+function getFileObjs(from = '0', to = '0') {
+    return Object.values(appData.fileObjs).filter(fileObj => {
             const key = fileObj.getActiveDate();
             return (key.substr(0, from.length) >= from && key.substr(0, to.length) <= to)
         }).sort((a, b) => a.getActiveDate() - b.getActiveDate());
 }
 
-DataController.prototype.getFile = async function(fileRef) {
+async function getFile(fileRef) {
     try {
         let file = await readFile(fileRef);
         if (!file) {
@@ -98,18 +68,16 @@ DataController.prototype.getFile = async function(fileRef) {
         errorHandler(e);
     }
 }
-DataController.prototype.findFirstFileObj = function() {
-    const fileObjs = Object.values(this.appData.fileObjs).sort((a, b) => a.getActiveDate() - b.getActiveDate());
+function findFirstFileObj() {
+    const fileObjs = Object.values(appData.fileObjs).sort((a, b) => a.getActiveDate() - b.getActiveDate());
     return fileObjs[0];
 }
 
-DataController.prototype.removeFiles = function(fileObjs) {
+function removeFiles(fileObjs) {
     fileObjs.forEach(fileObj => {
         let key = fileObj.uid;
-        if (this.appData.fileObjs[key]) {
-        this.appData.delete(key);
-        this.updateList();
-        this.setStatus('Deleted');
+        if (appData.fileObjs[key]) {
+        appData.delete(key);
         return true
     }
     return Error(`no such file under key: ${key}`)
@@ -132,3 +100,5 @@ function checkFileType(type, name) {
     }
     return type
 }
+
+export {addFiles, listNodes, getFileObjs, getFile, removeFiles}
