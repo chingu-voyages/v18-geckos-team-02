aaf68fc3df7a9e7a202b8c90754fde7ea1105a79
Jon Hill <jondhill1984@gmail.com>
2020-05-07 17:18:24 +0300

    basic addition of buttons into timeline

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import styled  from 'styled-components';
 import plusSign from '../assets/plusSign.svg';
-import minusSign from '../assets/minusSign2.svg'
+import minusSign from '../assets/minusSign2.svg';
+import EditButton from  '../assets/trashIcon.svg';
+import ImportButton from '../assets/importButton.svg';
+import ExportButton from '../assets/exportButton.svg';
+
 
 const NavBarContainer = styled.div`
     display: flex;
@@ -29,12 +33,28 @@ const TogglenModalButton = styled.div`
     justify-content: center;
 `;
 
-const EditModeButton = styled.div`
+const EditModeButton = styled.img`
     display: flex;
     height: 60px;
     width: 60px;
     align-items: center;
     justify-content: center;
+`;
+
+const ExportButtonContainer = styled.img`
+  display: flex;
+  height: 60px;
+  width: 60px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ImportButtonContainer = styled.img`
+  display: flex;
+  height: 60px;
+  width: 60px;
+  align-items: center;
+  justify-content: center;
 `;
 
 function NavBar({showUploads, setShowUploads, editMode, setEditMode}) {
@@ -45,12 +65,13 @@ function NavBar({showUploads, setShowUploads, editMode, setEditMode}) {
             {
               !showUploads ?
               <img src={plusSign} alt="toggle upload modal open button" /> : 
-              <img  src={minusSign} alt="toggle upload modal open button" />  
+              <img  src={minusSign} alt="toggle upload modal closed button" />  
             } 
           </TogglenModalButton>
-          <EditModeButton onClick={() => setEditMode(!editMode)}>
-            E
-          </EditModeButton>
+          <EditModeButton src={EditButton} onClick={() => setEditMode(!editMode)} />
+          <ImportButtonContainer src={ImportButton} alt="import a timline button" />
+          <ExportButtonContainer src={ExportButton} alt="export a timline button" />
+
         </ButtonContainer>
     </NavBarContainer>
   );
