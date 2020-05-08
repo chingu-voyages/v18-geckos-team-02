a801804514146012a30392b5f58b1c700d71103f
jondhill333 <56552404+jondhill333@users.noreply.github.com>
2020-05-08 12:29:31 +0300

    Feature/import and export button (#142)
    * main hidden when upload modal open
    
    * moved modals down from top
    
    * basic addition of buttons into timeline
    
    * margin for icons and responsive resize for mobile
    
    * Add info titles to files
    - Save size on file upload
    - File style tweeks
    - Make time not repeat
    
    * Swap import button to input
    
    * basic addition of buttons into timeline
    
    * margin for icons and responsive resize for mobile
    
    Co-authored-by: Lily <46561378+lily-law@users.noreply.github.com>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styled  from 'styled-components';
 import plusSign from '../assets/plusSign.svg';
 import logo from './../../src/assets/wavy-logo.svg';
-import minusSign from '../assets/minusSign2.svg'
+import minusSign from '../assets/minusSign2.svg';
+import EditButton from  '../assets/trashIcon.svg';
+import ImportButton from '../assets/importButton.svg';
+import ExportButton from '../assets/exportButton.svg';
+import {exportTimeLine, importTimeLine} from '../services/dataController';
 
 const NavBarContainer = styled.div`
     display: flex;
@@ -22,43 +26,127 @@ const ButtonContainer = styled.div`
     align-items: center;
 `;
 
-const TogglenModalButton = styled.div`
+const ToggleModalButton = styled.div`
     display: flex;
     height: 60px;
     width: 60px;
     align-items: center;
     justify-content: center;
+    @media (max-width: 500px){
+      height: 40px;
+      width: 40px;
+    }
+`;
+
+const PlusImg = styled.img`
+    height: 60px;
+    width: 60px;
+    @media (max-width: 500px){
+      height: 40px;
+      width: 40px;
+    }
+`;
+
+const MinusImg = styled.img`
+    height: 60px;
+    width: 60px;
+    @media (max-width: 500px){
+      height: 40px;
+      width: 40px;
+    }
 `;
 const LogoWrapper = styled.div`
   position: absolute;
   left: 0;
 `;
 
-const EditModeButton = styled.div`
+const EditModeButton = styled.img`
     display: flex;
     height: 60px;
     width: 60px;
     align-items: center;
     justify-content: center;
+    margin-left: 5px;
+    @media (max-width: 500px){
+      height: 40px;
+      width: 40px;
+    }
+`;
+
+export const ImportButtonInput = styled.input`
+  border: 0;
+  clip: rect(0, 0, 0, 0);
+  height: 1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute !important;
+  white-space: nowrap;
+  width: 1px;
+`;
+
+export const ImportButtonLabel = styled.label`
+  height: 60px;
+  width: 60px;
+  align-items: center;
+  justify-content: center;
+  margin-left: 5px;
+  @media (max-width: 500px){
+    height: 40px;
+    width: 40px;
+  }
+`;
+
+const ImportButtonIcon = styled.img`
+  display: flex;
+  height: 60px;
+  width: 60px;
+  align-items: center;
+  justify-content: center;
+  @media (max-width: 500px){
+    height: 40px;
+    width: 40px;
+  }
+`;
+
+const ExportButtonContainer = styled.img`
+  display: flex;
+  height: 60px;
+  width: 60px;
+  align-items: center;
+  justify-content: center;
+  margin-left: 10px;
+  @media (max-width: 500px){
+    height: 40px;
+    width: 40px;
+  }
 `;
 
 function NavBar({showUploads, setShowUploads, editMode, setEditMode}) {
+  function handleOnChange(e) {
+    importTimeLine();
+    e.target.value = null;
+  }
+
   return (
     <NavBarContainer>
       <LogoWrapper>
         <img src={logo} alt="Wavy logo"/>
       </LogoWrapper>
         <ButtonContainer>
-          <TogglenModalButton onClick={() => setShowUploads(!showUploads)}>
+          <ToggleModalButton onClick={() => setShowUploads(!showUploads)}>
             {
               !showUploads ?
-              <img src={plusSign} alt="toggle upload modal open button" /> : 
-              <img  src={minusSign} alt="toggle upload modal open button" />  
+              <PlusImg src={plusSign} alt="toggle upload modal open button" /> : 
+              <MinusImg  src={minusSign} alt="toggle upload modal closed button" />  
             } 
-          </TogglenModalButton>
-          <EditModeButton onClick={() => setEditMode(!editMode)}>
-            E
-          </EditModeButton>
+          </ToggleModalButton>
+          <EditModeButton src={EditButton} onClick={() => setEditMode(!editMode)} />
+          <ImportButtonInput type="file" id="file" onChange={handleOnChange} multiple/>
+          <ImportButtonLabel htmlFor="file">
+            <ImportButtonIcon src={ImportButton} alt="import a timeline file" />
+          </ImportButtonLabel> 
+          <ExportButtonContainer src={ExportButton} onClick={exportTimeLine} alt="export a timline file" />
+
         </ButtonContainer>
     </NavBarContainer>
   );
