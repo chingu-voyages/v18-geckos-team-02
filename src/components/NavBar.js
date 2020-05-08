b78444bdf1c55fb063e73676816b10edd68f7418
Jon Hill <jondhill1984@gmail.com>
2020-05-08 11:30:53 +0300

    Swap import button to input

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled  from 'styled-components';
+
 import plusSign from '../assets/plusSign.svg';
 import minusSign from '../assets/minusSign2.svg';
 import EditButton from  '../assets/trashIcon.svg';
 import ImportButton from '../assets/importButton.svg';
 import ExportButton from '../assets/exportButton.svg';
+
+import {exportTimeLine, importTimeLine} from '../services/dataController';
 
 
 const NavBarContainer = styled.div`
@@ -70,18 +73,38 @@ const EditModeButton = styled.img`
     @media (max-width: 500px){
       height: 40px;
       width: 40px;
-    
-
 `;
 
-const ImportButtonContainer = styled.img`
-  display: flex;
+export const ImportButtonInput = styled.input`
+  border: 0;
+  clip: rect(0, 0, 0, 0);
+  height: 1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute !important;
+  white-space: nowrap;
+  width: 1px;
+`;
+
+export const ImportButtonLabel = styled.label`
   height: 60px;
   width: 60px;
   align-items: center;
   justify-content: center;
   margin-left: 5px;
 
+  @media (max-width: 500px){
+    height: 40px;
+    width: 40px;
+
+`;
+
+const ImportButtonIcon = styled.img`
+  display: flex;
+  height: 60px;
+  width: 60px;
+  align-items: center;
+  justify-content: center;
 
   @media (max-width: 500px){
     height: 40px;
@@ -102,9 +125,13 @@ const ExportButtonContainer = styled.img`
     width: 40px;
 `;
 
-
-
 function NavBar({showUploads, setShowUploads, editMode, setEditMode}) {
+
+  function handleOnChange(e) {
+    importTimeLine();
+    e.target.value = null;
+  }
+
   return (
     <NavBarContainer>
         <ButtonContainer>
@@ -116,8 +143,11 @@ function NavBar({showUploads, setShowUploads, editMode, setEditMode}) {
             } 
           </ToggleModalButton>
           <EditModeButton src={EditButton} onClick={() => setEditMode(!editMode)} />
-          <ImportButtonContainer src={ImportButton} alt="import a timline button" />
-          <ExportButtonContainer src={ExportButton} alt="export a timline button" />
+          <ImportButtonInput type="file" id="file" onChange={handleOnChange} multiple/>
+          <ImportButtonLabel htmlFor="file">
+            <ImportButtonIcon src={ImportButton} alt="import a timline button" />
+          </ImportButtonLabel> 
+          <ExportButtonContainer src={ExportButton} onClick={exportTimeLine} alt="export a timline button" />
 
         </ButtonContainer>
     </NavBarContainer>
