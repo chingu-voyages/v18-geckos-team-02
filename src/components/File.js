d9165be1c47efd8795a98cee609843bcd349acf9
Lily <46561378+lily-law@users.noreply.github.com>
2020-05-12 10:28:48 +0100

    Convert note upload to File - Change note type to 'text/plain'

--- src/components/File.js
+++ src/components/File.js
@@ -109,13 +109,18 @@ export default function File({fileObj, showTime, time, isMain}) {
         if (ref.current) {
             let observer = new IntersectionObserver(entries => {
                 if (entries[0].isIntersecting) {
-                    getFile(fileObj.fileRef).then(blob => {
-                        let url = null
+                    getFile(fileObj.fileRef).then(async blob => {
+                        let data = null;
                         if (blob) {
-                            url = URL.createObjectURL(blob);
+                            if (fileObj.type === 'text/plain') {
+                                data = await blob.text();
+                            }
+                            else {
+                                data = URL.createObjectURL(blob);
+                            }
                         }
-                        setFile(url);
-                        if (url === null) {
+                        setFile(data);
+                        if (data === null) {
                             fileObj.flagMissingData();
                         }
                         observer.unobserve(ref.current);
@@ -133,7 +138,7 @@ const info = `
 ${fileObj.name} 
 lastModifed: ${fileObj.timeStamps.modified} 
 ${fileObj.timeStamps.user ? 'userSetTime: '+fileObj.timeStamps.user+'\n' : ''}type: ${fileObj.type}
-${fileObj.tag?.length > 0 ? 'tags: '+fileObj.tags.join(' ')+'\n' : ''}${fileObj.size ? 'size: '+condenseSize(fileObj.size)+'\n' : ''}
+${fileObj.tags?.length > 0 ? 'tags: '+fileObj.tags.join(', ')+'\n' : ''}${fileObj.size ? 'size: '+condenseSize(fileObj.size)+'\n' : ''}
     
 `;
         if (props.enabled) {
@@ -159,7 +164,7 @@ ${fileObj.tag?.length > 0 ? 'tags: '+fileObj.tags.join(' ')+'\n' : ''}${fileObj.
                 <DownloadWrapper enabled={enableDownload}>
                     {fileObj.type.includes('image') ? 
                         <Img src={file} alt="" /> : 
-                        fileObj.type === 'note' ? <Note className="note"><h1>{fileObj.name}</h1><p>{fileObj.text}</p></Note> :
+                        fileObj.type === 'text/plain' ? <Note className="note"><h1>{fileObj.name}</h1><p>{file}</p></Note> :
                         <FileIcon {...charsToColour(fileObj.type)}>{fileObj.name}</FileIcon>
                     }
                 </DownloadWrapper>}
